856b08f12c71ed507228105bd81dee55958449c0
jas <uqjstupp@uq.edu.au>
2021-08-26 09:40:49 +1000

    removed cms

--- eox_studio/api/v1/urls.py
+++ eox_studio/api/v1/urls.py
@@ -8,15 +8,15 @@ from eox_studio.api.v1 import views
 app_name = 'eox_studio'  # pylint: disable=invalid-name
 
 urlpatterns = [  # pylint: disable=invalid-name
-    url(r'^user/$', views.EdxappUser.as_view(), name='edxapp-user'),
-    url(r'^enrollment/$', views.EdxappEnrollment.as_view(), name='edxapp-enrollment'),
-    url(r'^grade/$', views.EdxappGrade.as_view(), name='edxapp-grade'),
-    url(r'^pre-enrollment/$', views.EdxappPreEnrollment.as_view(), name='edxapp-pre-enrollment'),
-    url(r'^userinfo/$', views.UserInfo.as_view(), name='edxapp-userinfo'),
+    # url(r'^user/$', views.EdxappUser.as_view(), name='edxapp-user'),
+    # url(r'^enrollment/$', views.EdxappEnrollment.as_view(), name='edxapp-enrollment'),
+    # url(r'^grade/$', views.EdxappGrade.as_view(), name='edxapp-grade'),
+    # url(r'^pre-enrollment/$', views.EdxappPreEnrollment.as_view(), name='edxapp-pre-enrollment'),
+    # url(r'^userinfo/$', views.UserInfo.as_view(), name='edxapp-userinfo'),
     url(r'^course/$', views.EdxappCourse.as_view(), name='edxapp-courseinfo'),
 ]
 
 if getattr(settings, "EOX_CORE_ENABLE_UPDATE_USERS", None):
     urlpatterns += [
-        url(r'^update-user/$', views.EdxappUserUpdater.as_view(), name='edxapp-user-updater'),
+        # url(r'^update-user/$', views.EdxappUserUpdater.as_view(), name='edxapp-user-updater'),
     ]
